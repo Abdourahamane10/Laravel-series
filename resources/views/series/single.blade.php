e38777ad55472f4175e7617c3d8deb32c6e3f0de
Abdourahamane <amdroudjae@gmail.com>
2022-03-13 13:13:46 +0100

    Affichage des series dans  resources/views/series/

---
+++ resources/views/series/single.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+
+@section('content')
+    <p>{{ $serie->content }}</p><br>
+    <h3>url : <a href="{{ $serie->url }}">{{ $serie->url }}</a></h3><br>
+    <div class="pied de page">
+        <footer>
+            <p>&copy; Auteur : {{ $serie->author->name }}</p>
+        </footer>
+    </div>
+@endsection
