46cebdde21dda19cfcbe57854923f1251b1e1514
Abdourahamane <amdroudjae@gmail.com>
2022-03-24 17:47:34 +0100

    Comment model modified by establishing the relationship between the tables comments, user and series

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that authored the comment.
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    /**
+     * Get the serie that authored the comment.
+     */
+    public function series()
+    {
+        return $this->belongsTo(Serie::class, 'serie_id');
+    }
 }
