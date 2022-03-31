94d13a5580c0253abac5518b86cd8d48ec9f8dc4
Abdourahamane <amdroudjae@gmail.com>
2022-03-31 18:27:36 +0200

    add form for comments and comments list

--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -11,7 +11,38 @@
                         href="{{ route('series.show', ['id' => $serie->id]) }}">{{ $serie->title }}</a></li>
             @endforeach
         </ul>
-    </div><br>
+    </div><br><br>
 
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<div>
+    <form action="{{ url('/series') }}" method="POST">
+        @csrf
+        <label for="content">Ajouter un commentaire :</label>
+        <textarea id="content" name="content" cols="30" rows="10" placeholder="commentaires..."></textarea><br>
+        <button type="submit">envoyer</button>
+    </form>
+</div><br><br>
+
+
+<div>
+    <h1>Commentaires :</h1><br>
+
+    @foreach ($comments as $comment)
+        <p>Titre de la série : {{ \App\Models\Serie::where('id',$comment->serie_id)->first()->title }}</p>
+        <p>Commentaire : {{ $comment->content }}</p>
+        <p>&copy; Author : {{ \App\Models\User::where('id',$comment->author_id)->first()->name }}</p><br>
+
+    @endforeach
+</div>
 
 @endsection
