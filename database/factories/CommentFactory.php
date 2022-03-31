16cc917fed8dfa60bddc46ab6f6934002b79091e
Abdourahamane <amdroudjae@gmail.com>
2022-03-31 18:16:55 +0200

    create CommentFactory and complete definition function

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Serie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'author_id' => User::inRandomOrder()->first()->id,
+            'serie_id' => Serie::inRandomOrder()->first()->id,
+            'content' => $this->faker->sentences,
+            'date' => $this->faker->dateTime,
+        ];
+    }
+}
