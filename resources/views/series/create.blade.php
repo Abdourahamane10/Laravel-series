1828e2bb51acaa66980f97c566c9457aef31ad17
Abdourahamane <amdroudjae@gmail.com>
2022-04-03 11:18:39 +0200

    Added tag and status lists to select and a media selection field for the series to add in the add series form in resources/views/series/create.blade.php

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -14,20 +14,35 @@
     @endif
 
     <div class="form">
-        <form action="{{ url('admin/series') }}" method="POST">
+        <form action="{{ url('admin/series') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <label for="title">title :</label>
             <input type="text" id="title" name="title"><br>
             <label for="content">content :</label>
-            <textarea id="content" name="content" cols="30" rows="10" @class(['p-4', 'font-bold' => true])></textarea><br>
+            <textarea id="content" name="content" cols="30" rows="10"></textarea><br>
             <label for="acteurs">acteurs :</label>
-            <textarea id="acteurs" name="acteurs" cols="15" rows="10" @class(['p-4', 'font-bold' => true])></textarea><br>
+            <textarea id="acteurs" name="acteurs" cols="15" rows="10")></textarea><br>
             <label for="url">url :</label>
             <input type="url" id="url" name="url" placeholder="https://example.com" pattern="https://.*" size="30"><br>
             <label for="tags">tags :</label>
-            <textarea id="tags" name="tags" cols="20" rows="10" @class(['p-4', 'font-bold' => true])></textarea><br>
+            <select id="tags" name="tags">
+                <option value="action">Action</option>
+                <option value="humour">Humour</option>
+                <option value="amour">Amour</option>
+                <option value="drama">Drama</option>
+                <option value="policier">Policier</option>
+                <option value="comique">Comique</option>
+            </select><br>
             <label for="status">status :</label>
-            <input type="text" id="status" name="status"><br>
+            <select id="status" name="status">
+                <option value="En cours">En cours</option>
+                <option value="Publiée">Publié</option>
+                <option value="Modifiée">Modifié</option>
+            </select>
+            <label for="file">Choose a profile picture:</label>
+            <input type="file"
+            id="file" name="file" accept="image/png, image/jpeg"><br>
+
             <button type="submit">Créer</button>
         </form>
     </div>
