bc4e93e4c068b8ef18d0232a62550a58908c1719
Abdourahamane <amdroudjae@gmail.com>
2022-04-03 11:20:22 +0200

    Display of medis, actors and authors of the series

--- resources/views/series/single.blade.php
+++ resources/views/series/single.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts/main')
 
 @section('content')
-    <p>Title : {{ $serie->title }}</p>
+    <p> @if ($serie->media != "")
+        <a href="#"><img src="{{url('/storage/media/'.$serie->media.'.jpg')}}" alt="" height="200" width="200"/></a><br>
+        @else
+        <a href="#"><img src="{{url('/storage/media/mediaDefault.png')}}" alt="" height="200" width="200"/></a><br>
+        @endif</p>
+    <p>Title : {{ $serie->title }}</p><br>
     <p> Content : {{ $serie->content }}</p><br>
     <h3>url : <a href="{{ $serie->url }}">{{ $serie->url }}</a></h3><br>
     <div class="pied de page">
