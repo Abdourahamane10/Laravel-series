2ad564eafcca927f17f969d0659608d7d9b74fbd
Abdourahamane <amdroudjae@gmail.com>
2022-04-03 11:12:01 +0200

    modified SerieFactory definition function by defining tag and status lists to be automatically generated and by adding an automatic generation of series media

--- database/factories/SerieFactory.php
+++ database/factories/SerieFactory.php
@@ -14,15 +14,23 @@ class SerieFactory extends Factory
      */
     public function definition()
     {
+        $listOfActors = "";
+        for ($i = 1; $i <= random_int(5, 10); $i++) {
+            $listOfActors .= $this->faker->unique()->name() . ',';
+        }
+        $len = strlen($listOfActors);
+        $listOfActors = substr($listOfActors, 0, $len);
+
         return [
             'author_id' => User::inRandomOrder()->first()->id,
             'title' => $this->faker->realTextBetween($min = 8, $max = 20),
             'content' => $this->faker->realTextBetween($min = 500, $max = 1000),
-            'acteurs' => $this->faker->name,
+            'acteurs' => $listOfActors,
             'url' => $this->faker->url,
-            'tags' => $this->faker->words(5, true),
+            'tags' => $this->faker->randomElement(['Action', 'Humour', 'Policier', 'Comique', 'Amour', 'Drama']),
             'date' => $this->faker->dateTime,
-            'status' => $this->faker->sentence,
+            'status' => $this->faker->randomElement(['En cours', 'Publiée', 'Modifiée']),
+            'media' => $this->faker->randomElement(['media1', 'media2', 'media3', 'media4', 'media5', 'media6', 'media7', 'media8', 'media9', 'media10', 'media11', 'media12', 'media13', 'media14', 'media15', 'media16', 'media17', 'media18', 'media19']),
         ];
     }
 }
