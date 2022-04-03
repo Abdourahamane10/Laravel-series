db52d3b5207adc19bb90ac7a6692d2ea1cdc73c6
Abdourahamane <amdroudjae@gmail.com>
2022-04-03 11:22:15 +0200

    Display of medis, actors and authors of the series in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,20 @@
     <h2>Bienvenu dans le site Laravel Séries</h2>
     <br>
     <!--{{-- > //$series <! --}}-->
-    <ul>
+    <ul class="utl">
         @foreach ($series as $serie)
-            <li><a href="{{ route('series.show', ['id' => $serie->id]) }}">{{ $serie->title }}</a></li>
+            <li> @if ($serie->media != "")
+                <a href="#"><img src="{{url('/storage/media/'.$serie->media.'.jpg')}}" alt="" height="200" width="200"/></a><br>
+                @else
+                <a href="#"><img src="{{url('/storage/media/mediaDefault.png')}}" alt="" height="200" width="200"/></a><br>
+                @endif
+
+                <div>
+                  <a href="{{ route('series.show', ['id' => $serie->id]) }}">Title : {{ $serie->title }}</a><br>
+                  <p>Acteurs : {{ $serie->acteurs }}</p><br>
+                  <p>&copy; Auteur : {{ $serie->author->name }}</p><br><br>
+                </div>
+            </li>
         @endforeach
     </ul>
 @endsection
