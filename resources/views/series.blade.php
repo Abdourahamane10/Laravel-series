6bacb85ecc47668d9b71729487e683e48b9a370c
Abdourahamane <amdroudjae@gmail.com>
2022-04-11 13:28:11 +0200

    deletion of comments in the series view

--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -5,8 +5,13 @@
 
     <div>
         <livewire:search />
-        <ul class="utl">
+
             @foreach ($series as $serie)
+            <?php
+            $rating = \App\Models\Comment::where('serie_id',$serie->id)->avg('rating');
+            ?>
+
+            <ul class="utl">
                 <li class="relative">
                     @if ($serie->media != "")
                     <a href="#"><img src="{{url('/media/'.$serie->media.'.jpg')}}" alt="media" height="200" width="200"/></a><br>
@@ -14,46 +19,18 @@
                     <a href="#"><img src="{{url('/media/mediaDefault.jpg')}}" alt="media" height="200" width="200"/></a><br>
                 @endif
 
-                    <div>
+                    <div class="right-part">
                       <a href="{{ route('series.show', ['id' => $serie->id]) }}">Titre : {{ $serie->title }}</a><br>
+                      <p> <div class="stars"><div class="percent" style="width: {{  ($rating*20).'%' }};"></div></div></p>
+                    <br/><br/>
                       <p>Acteurs : {{ $serie->acteurs }}</p><br>
                       <p>&copy; Auteur : {{ $serie->author->name }}</p>
                     </div>
                 </li><br><br>
+            </ul>
             @endforeach
-        </ul>
-    </div><br><br>
 
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
     </div>
-@endif
 
-<div>
-    <form action="{{ url('/series') }}" method="POST">
-        @csrf
-        <label for="content">Ajouter un commentaire :</label>
-        <textarea id="content" name="content" cols="30" rows="10" placeholder="commentaires..."></textarea><br>
-        <button type="submit">envoyer</button>
-    </form>
-</div><br><br>
-
-
-<div>
-    <h1>Commentaires :</h1><br>
-
-    @foreach ($comments as $comment)
-        <p>Titre de la série : {{ \App\Models\Serie::where('id',$comment->serie_id)->first()->title }}</p>
-        <p>Commentaire : {{ $comment->content }}</p>
-        <p>&copy; Author : {{ \App\Models\User::where('id',$comment->author_id)->first()->name }}</p><br>
-
-    @endforeach
-</div>
 
 @endsection
